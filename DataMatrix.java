92a9c3b58ec50647293ffab30c1b8894e4a9052f
JJhello <>
2021-11-20 03:04:57 +0000

    .

--- DataMatrix.java
+++ DataMatrix.java
@@ -4,6 +4,24 @@
 class DataMatrix 
 {
 
+public static final char BLACK_CHAR = '*';
+public static final char WHITE_CHAR = ' '; 
 
+// private variables
+private BarcodeImage image;
+private String text;
+private int actualWidth;
+private int actualHeight;
+
+public DataMatrix()
+{
   
 }
+
+public DataMatrix(BarcodeImage image)
+{
+
+}
+
+
+} // end of Datamatrix
