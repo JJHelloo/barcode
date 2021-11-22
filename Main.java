333ed8cc1274834681a3f3e6d295485cb6f8fab8
Jolene Farley <>
2021-11-22 01:34:08 +0000

    Removed testing from main

--- Main.java
+++ Main.java
@@ -8,30 +8,6 @@ class Main
 {
   public static void main(String[] args)
   {
-    //Some testing - Jolene
-    BarcodeImage bc = new BarcodeImage();
-    bc.displayToConsole();
 
-    System.out.println();
-
-    BarcodeImage bcCopy = null;
-
-    try
-    {
-      bcCopy = (BarcodeImage)bc.clone();
-    }
-    catch (CloneNotSupportedException e)
-    {
-      bcCopy = null;
-    }
-    
-    bcCopy.displayToConsole();
-
-    bcCopy.setPixel(0, 0, true);
-
-    System.out.println("Testing true in first at 0,0");
-    bc.displayToConsole();
-    System.out.println();
-    bcCopy.displayToConsole();
   }
 }
